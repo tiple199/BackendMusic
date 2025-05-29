5ed605282ca1edb0ce3f0c12cd7f31b22e29b088
HuuPhuocBui <135402077+HuuPhuocBui@users.noreply.github.com>
2025-05-29 09:16:05 +0700

    update trang chu 4

--- src/router/baihat.js
+++ src/router/baihat.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { addSong, getAllSongs, getSongById } from "../controllers/baihat.js";
+import { addSong, getAllSongs, getSongById, getSongsByTheLoai, getSongsByArtist, getSongsByAlbum, searchSongs, getTopLikedSongs} from "../controllers/baihat.js";
 
 const router = express.Router();
+router.get('/songs/by-theloai/:theLoaiId', getSongsByTheLoai);
+router.get("/songs/byArtist/:artistId", getSongsByArtist);
+router.get("/songs/byAlbum/:albumId", getSongsByAlbum);
+router.get('/songs/search', searchSongs); 
+router.get("/songs/top-liked-songs", getTopLikedSongs);
 router.get('/songs',getAllSongs);
 router.post('/songs',addSong);
 router.get('/songs/:id',getSongById);
+
 
 
 export default router;
